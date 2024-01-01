55c6a3e60f35b538c53957bec4ef4d89b49c15c1
sazzadbytes <sazzadbytes@gmail.com>
2024-01-01 17:27:57 +0600

    Conversion problem Solved

--- calculator/views.py
+++ calculator/views.py
@@ -1,27 +1,34 @@
 from django.shortcuts import render
+from decimal import Decimal
 
 # Create your views here.
 
 def index(request):
-    answer={}
-    ans=0
+    answer = {}
+    ans = 0
     try:
-        if request.method=='POST':
-            Base=int(request.POST.get('base'))
-            Ex=int(request.POST.get('ex'))
-            Div=int(request.POST.get('div'))
-            ans=pow(Base, Ex ,Div)
-            answer={
-                'ans':ans,
-                'base':Base,
-                'ex':Ex,
-                'div':Div
+        if request.method == 'POST':
+            base_str = request.POST.get('base')
+            ex_str = request.POST.get('ex')
+            div_str = request.POST.get('div')
+
+            try:
+                base = Decimal(base_str)
+                ex = Decimal(ex_str)
+                div = Decimal(div_str)
+                ans = pow(base, ex, div)  
+            except InvalidOperation:
+                answer = {'error': 'Invalid input values'}
+                return render(request, 'index.html', answer)
+
+            answer = {
+                'ans': ans,
+                'base': base,
+                'ex': ex,
+                'div': div
             }
 
-            
- 
-    except:
-        pass
+    except Exception as e:
+        answer = {'error': f'An error occurred: {e}'}
 
-    return render(request,'index.html',answer)
-
+    return render(request, 'index.html', answer)
